ff2b25a9986002deaeff1ba10e658b36d4e57d00
JosephVang099 <joseph-vang@outlook.com>
2023-01-30 10:22:13 -0500

    Update veryhard.js

--- Very_Hard/veryhard.js
+++ Very_Hard/veryhard.js
@@ -52,53 +52,36 @@ class programmer1 {
             console.log(this.name + 'would love to take on a new responsibility')
         }
     }
+    learnLanguage(language) { // (#7)
+        this.languages.push(language);
+    }
+    listLanguages() {
+        console.log(this.name + ' knows: ' + this.language);
+}
 }
 
 
 
 
 
-    class programmer1 extends person1 {
-    constructor(name, job, age, languages, busy = true) {
-    super(name, job, age,)
-    this.languages = languages;
-    this.busy = busy;
-    }
-    completeTask() {
-    this.busy = false;
-    }
-    acceptNewtask() {
-    this.busy = true;
-    }
-    offerNewtask() {
-    if (this.busy === true) {
-    console.log(`${this.name} can't accept any new tasks right now.`)
-    } 
-    else {
-    console.log(`${this.name} would love to take on a new responsibility`)
-    }
-    }
-    learnLanguage() {
-    for (let i = 0; i < arguments.length; i++) {
-    this.languages.push(arguments[i]);
-    }
-    }
-    listLanguages() {
-    console.log(`${this.name} knows the following languages: ${this.languages.join(", ")}`)
-    }
-    }
+    
 
 
-    const techProgrammer1 = new programmer1('Brad', 'back-end developer', 23, ['Html', 'Css', 'Javascript', 'French', 'English']);
-    const techProgrammer2 = new programmer1('Robert', 'Nurse', 22, ['Css', 'Javascript', 'Html', 'React'], false);
-    techProgrammer1.offerNewtask(); // busy
-    techProgrammer2.offerNewtask(); // not busy
-    techProgrammer1.completeTask();
-    techProgrammer2.completeTask();
-    techProgrammer1.offerNewtask(); // show now accept a task
-    techProgrammer2.offerNewtask(); // should be no change because they were already not busy
-    techProgrammer1.listLanguages(); // should have no languages
-    techProgrammer2.listLanguages(); // should have languages
-    techProgrammer1.learnLanguage("Spanish", "French", "German");
-    techProgrammer1.listLanguages();
-    techProgrammer2.listLanguages();
+   const person1 = new person('Brad', 'back-end developer', 23, ['Html', 'Css', 'Javascript', 'French', 'English']);
+
+    const programmer1 = new programmer('Robert', 'front-end developer', 22, ['Css', 'Javascript', 'Html', 'React'], false);
+    person1.offerNewtask(); // busy
+    programmer1.offerNewtask(); // not busy
+
+    person1.completeTask();
+    programmer1.completeTask();
+
+    person1.offerNewtask(); // show now accept a task
+    programmer1.offerNewtask(); // should be no change because they were already not busy
+
+    person1.listLanguages(); // should have no languages
+    programmer1.listLanguages(); // should have languages
+
+    person1.learnLanguage("Spanish", "French", "German");
+    person1.listLanguages();
+    programmer1.listLanguages();
